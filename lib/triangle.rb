513702cc55cd11497297baab42b1b85020c1f6b1
Michael Dally <mddally@gmail.com>
2022-03-22 13:59:04 -0400

    commit tree 8f39719f9ae28e70d33a45a35b15738d90e6af55

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if isValid? self
+      if(self.s1 == self.s2)
+        if(self.s1 == s3)
+         return :equilateral
+        else
+          return :isosceles
+        end
+      elsif(self.s1 == s3)
+        return :isosceles
+      elsif(self.s2 == self.s3)
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  def isValid? triangle
+    (triangle.s1 > 0 && 
+      triangle.s2 > 0 &&
+      triangle.s3 > 0 &&
+      triangle.s1 + triangle.s2 > triangle.s3 &&
+      triangle.s1 + triangle.s3 > triangle.s2 &&
+      triangle.s2 + triangle.s3 > triangle.s1
+    )
+  end
+
+  class TriangleError < StandardError
+    def message
+      "That is not a traingle"
+    end
+  end
 end
+
+puts Triangle.new(5,5,5).kind
+puts Triangle.new(5,4,5).kind
+puts Triangle.new(5,4,3).kind
+puts Triangle.new(50,4,3).kind
+puts Triangle.new(50,4,3).kind
